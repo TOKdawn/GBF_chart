5e7e887688512ca92e49fe06bf0e44a908fa4d3d
Zhangpengyu <18746442232@163.com>
2020-08-07 16:38:31 +0800

    构建项目

---
+++ GBF-chart/src/App.vue
@@ -0,0 +1,105 @@
+<style scoped>
+
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+    }
+    .layout-breadcrumb{
+        padding: 10px 15px 0;
+    }
+    .layout-content{
+        min-height: 800px;
+        margin: 15px;
+        overflow: hidden;
+        background: #fff;
+        border-radius: 4px;
+    }
+    .layout-content-main{
+        padding: 10px;
+    }
+    .layout-copy{
+        text-align: center;
+        padding: 10px 0 20px;
+        color: #9ea7b4;
+    }
+    .layout-menu-left{
+        background: #464c5b;
+    }
+    .layout-header{
+        height: 60px;
+        background: #fff;
+        box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    }
+    .layout-logo-left{
+        width: 90%;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        margin: 0px auto  15px;
+        line-height: 30px;
+        color: rgb(255, 205, 86);
+        font-size: 16px;
+        font-weight: 500;
+        text-align: center;
+    }
+</style>
+<template>
+    <div class="layout">
+        <Row type="flex">
+            <i-col span="5" class="layout-menu-left">
+                <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
+                    <div class="layout-logo-left"> 目录</div>
+                    <Submenu name="1">
+                        <template slot="title">
+                            <Icon type="ios-navigate"></Icon>
+                            雷达图
+                        </template>
+                        <Menu-item name="1-1">选项 1</Menu-item>
+                        <Menu-item name="1-2">选项 2</Menu-item>
+                        <Menu-item name="1-3">选项 3</Menu-item>
+                    </Submenu>
+                    <Submenu name="2">
+                        <template slot="title">
+                            <Icon type="ios-keypad"></Icon>
+                            导航二
+                        </template>
+                        <Menu-item name="2-1">选项 1</Menu-item>
+                        <Menu-item name="2-2">选项 2</Menu-item>
+                    </Submenu>
+                    <Submenu name="3">
+                        <template slot="title">
+                            <Icon type="ios-analytics"></Icon>
+                            导航三
+                        </template>
+                        <Menu-item name="3-1">选项 1</Menu-item>
+                        <Menu-item name="3-2">选项 2</Menu-item>
+                    </Submenu>
+                </Menu>
+            </i-col>
+            <i-col span="19">
+                <div class="layout-header"></div>
+                <div class="layout-breadcrumb">
+                </div>
+                <div class="layout-content">
+                  <router-view></router-view>
+                </div>
+                <div class="layout-copy">
+                    2011-2016 &copy; TalkingData
+                </div>
+            </i-col>
+        </Row>
+    </div>
+</template>
+<script>
+    export default {
+        
+    }
+// red: "rgb(255, 99, 132)"
+// orange: "rgb(255, 159, 64)"
+// yellow: "rgb(255, 205, 86)"
+// green: "rgb(75, 192, 192)"
+// blue: "rgb(54, 162, 235)"
+// purple: "rgb(153, 102, 255)"
+// grey: "rgb(201, 203, 207)"
+</script>
